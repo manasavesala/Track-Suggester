c1cafe60b88378b6ae0c93302633678ec68e8721
Manasa Vesala <vesalamanasa@gmail.com>
2018-10-19 11:43:26 -0700

    Added some styles in css and branching in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,51 @@
 $(document).ready(function() {
   $("form#track").submit(function(event) {
     event.preventDefault();
+    var Language = $("#language").val();
     var UIimp = $("input:radio[name=interest]:checked").val();
     var Company = $("input:radio[name=company]:checked").val();
     var Development = $("input:radio[name=development]:checked").val();
     var Learn = $("input:radio[name=learn]:checked").val();
-    var Language = $("input:radio[name=language]:checked").val();
+    var Demand = $("input:radio[name=demand]:checked").val();
 
     
     console.log(UIimp);
     console.log(Company);
     console.log(Development);
     console.log(Learn);
-    console.language(Language);
+    console.log(Demand);
+    console.log(Language);
 
     if(UIimp === "veryimp" && Company === "bigcmpy"){
       $("#ruby").show();
+      $("#css").show();
       $("#csharp").hide();
       $("#php").hide();
       $("#java").hide();
-    }else if(Company === "bigcmpy" && UIimp === "prettyimp"){
-      $("#java").show();
+      $("#design").hide();
+    }else if(Company === "bigcmpy" && UIimp === "someimp"){
+      $("#csharp").show();
+      $("#design").show();
       $("#php").hide();
       $("#java").hide();
-      $("#csharp").hide();
+      $("#ruby").hide();
+      $("css").hide();
     }else if(UIimp === "mostimp" || Learn === "content" || Company === "startup" || Development === "money"){
       $("#php").show();
+      $("#design").show();
       $("#csharp").hide();
       $("#ruby").hide();
       $("#java").hide();
-    }else if(Language === "cs"){
-      $("#php").shoe();
+      $("#css").hide();
+    }else if(Language === "cs" || Language === "wd"){
+      $("#design").show();
+      $("#css").show();
       $("#csharp").hide();
       $("#ruby").hide();
+      $("#php").hide();
       $("#java").hide();
     }
+
 
 
   
