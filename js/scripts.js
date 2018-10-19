8030be1b067dbe6ee1ed59f2a396f76f67258a58
Manasa Vesala <vesalamanasa@gmail.com>
2018-10-19 10:49:47 -0700

    Added some content in html,branching in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,41 @@ $(document).ready(function() {
   $("form#track").submit(function(event) {
     event.preventDefault();
     var UIimp = $("input:radio[name=interest]:checked").val();
-    console.log(UIimp);
-    if(UIimp === "prettyimportant"){
-      $("#java").show();
-    }
+    var Company = $("input:radio[name=company]:checked").val();
+    var Development = $("input:radio[name=development]:checked").val();
+    var Learn = $("input:radio[name=learn]:checked").val();
+    var Language = $("input:radio[name=language]:checked").val();
+
     
+    console.log(UIimp);
+    console.log(Company);
+    console.log(Development);
+    console.log(Learn);
+    console.language(Language);
+
+    if(UIimp === "veryimp" && Company === "bigcmpy"){
+      $("#ruby").show();
+      $("#csharp").hide();
+      $("#php").hide();
+      $("#java").hide();
+    }else if(Company === "bigcmpy" && UIimp === "prettyimp"){
+      $("#java").show();
+      $("#php").hide();
+      $("#java").hide();
+      $("#csharp").hide();
+    }else if(UIimp === "mostimp" || Learn === "content" || Company === "startup" || Development === "money"){
+      $("#php").show();
+      $("#csharp").hide();
+      $("#ruby").hide();
+      $("#java").hide();
+    }else if(Language === "cs"){
+      $("#php").shoe();
+      $("#csharp").hide();
+      $("#ruby").hide();
+      $("#java").hide();
+    }
+
+
   
   });
 });
