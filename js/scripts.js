8aaa3c9acdf32e34dae7c5850863b1ceb0e23b62
Manasa Vesala <vesalamanasa@gmail.com>
2018-10-19 15:17:27 -0700

    added some styles in css and added bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -7,54 +7,92 @@ $(document).ready(function() {
     var Development = $("input:radio[name=development]:checked").val();
     var Learn = $("input:radio[name=learn]:checked").val();
     var Demand = $("input:radio[name=demand]:checked").val();
+    var Interest = $("input:radio[name=interest]:checked").val();
     var Name = $("#name").val();
     
-    console.log(UIimp);
-    console.log(Company);
-    console.log(Development);
-    console.log(Learn);
-    console.log(Demand);
-    console.log(Language);
     $(".person").text(Name);
     if(Name === ""){
       alert("Please,Enter Your Name.");
     }else if(Language === "ja"){
       $(".sug").show();
       $("#java").show();
-    }else if(UIimp === "veryimp" && Company === "bigcmpy"){
-      $(".sug").show();
-      $("#ruby").show();
-      $("#css").show();
+/*
+
       $("#csharp").hide();
-      $("#php").hide();
-      $("#java").hide();
-      $("#design").hide();
-    }else if(Company === "bigcmpy" && UIimp === "someimp"){
-      $("#sug").show();
-      $("#csharp").show();
-      $("#css").show();
-      $("#php").hide();
-      $("#java").hide();
-      $("#ruby").hide();
-      $("#design").hide();
-    }else if(UIimp === "mostimp" || Learn === "content" || Company === "startup" || Development === "money"){
-      $("#php").show();
-      $("#design").show();
-      $("#csharp").hide();
-      $("#ruby").hide();
-      $("#java").hide();
       $("#css").hide();
-    }else if(Language === "cs" || Language === "wd"){
-      $("#design").show();
-      $("#css").show();
-      $("#csharp").hide();
       $("#ruby").hide();
       $("#php").hide();
+      $("#design").hide();
+      $("#javascript").hide(); 
+      $("#bootstrap").hide(); 
+*/
+    }else if(Language === "c"){
+      $(".sug").show();
+      $("#csharp").show();
+/*
+
+      $("#css").hide();
+      $("#php").hide();
       $("#java").hide();
+      $("#ruby").hide();
+      $("#design").hide();
+      $("#javascript").hide(); 
+      $("#bootstrap").hide(); 
+*/
+    }else if(Language === "cs"){
+      $(".sug").show();
+      $("#css").show();
+/*
+      $("#csharp").hide();
+      $("#php").hide();
+      $("#ruby").hide();
+      $("#java").hide();
+      $("#design").hide(); 
+      $("#javascript").hide(); 
+      $("#bootstrap").hide(); 
+*/
     }
+  });
+});
 
 
 
-  
+$(document).ready(function() {
+  $("form#track").submit(function(event) {
+    event.preventDefault();
+    var Language = $("#language").val();
+    var UIimp = $("input:radio[name=interest]:checked").val();
+    var Company = $("input:radio[name=company]:checked").val();
+    var Development = $("input:radio[name=development]:checked").val();
+    var Learn = $("input:radio[name=learn]:checked").val();
+    var Demand = $("input:radio[name=demand]:checked").val();
+    var Interest = $("input:radio[name=interest]:checked").val();
+
+    if(Development === "datarichapp" || Development === "design" || Company === "startup" || Company === "contentcmpy"){
+      $("#php").show();
+    }else if(Development === "program" || Development === "money" || Company === "bigcmpy"){
+      $("#ruby").show();
+    }
+  });
+});
+
+$(document).ready(function() {
+  $("form#track").submit(function(event) {
+    event.preventDefault();
+    var Language = $("#language").val();
+    var UIimp = $("input:radio[name=interest]:checked").val();
+    var Company = $("input:radio[name=company]:checked").val();
+    var Development = $("input:radio[name=development]:checked").val();
+    var Learn = $("input:radio[name=learn]:checked").val();
+    var Demand = $("input:radio[name=demand]:checked").val();
+    var Interest = $("input:radio[name=interest]:checked").val();
+
+    if(Learn === "coding" || Learn === "webpage" || Demand === "useful"|| Demand === "quick"){
+      $("#javascript").show();
+    }else if(Learn === "content" || Demand === "enjoy" || Interest === "someimp"){
+      $("#bootstrap").show();
+    }else if(Interest === "veryimp" || Interest === "mostimp"){
+      $("#css").show();
+    }
   });
 });
